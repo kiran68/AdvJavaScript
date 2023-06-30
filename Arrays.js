6af8b96a7d14f3e1677b862d62a9df8abdea3a43
kiran68 <kiranurshil88@gmail.com>
2023-06-30 19:13:22 +0530

    Add: Diffrent types of arrya

---
+++ Arrays.js
@@ -0,0 +1,24 @@
+//Array is the special variable which can hold more  than one value
+
+const car = ["Toyota" , "Maruti" , "Audi"];
+
+//spaces and line breaks are not important.
+
+const cars1 = [
+    "Saab" , "Volvo" , "BMW"
+
+];
+// we can also create an array, and then provide the elements
+
+const cars = [];
+cars[0] = "GTR";
+cars[1] = "Volvo";
+cars[2] = "BMW";
+
+// Using New Keyword
+
+const cars = new Array("GTR" , "Volvo", "BMW");
+
+//Accessing Array Elements 
+const cars = ["Saab", "Volvo" , "BMW"];
+let car1 = cars[0];
