7ed232015bd78db084ff3b4b77a50ae14868308a
kiran68 <kiranurshil88@gmail.com>
2023-07-02 16:26:04 +0530

    Add: Synchronous And Asynchronous Examples

--- Synchronous.js
+++ Synchronous.js
@@ -2,6 +2,9 @@
 //the javaScript engine executes our program sequentially, one line at a time from 
 //top to bottom in the exact order of the statements.
 
+//1) instruction in synchronous code executes in a given sequence
+//2) each operation waits for the previous operation to complete its execution
+//3) most of the time javascript is used as synchronous code 
 
 var a = 1;
 var b= 2;
@@ -25,12 +28,26 @@ const greetOne = makeGreet(nameOne);
 console.log(greetOne);
 
 
+const someFunction = function () {
+    console.log("Inside someFunction");
+    console.log("Do Something");
+
+};
+console.log("Start");
+someFunction();
+console.log("End");
+
+
 
 //-----------------------------------------------------------
 
 //Asynchronous
 //function running in parallel with other functions are colled asynchronous a good example is javaScrip setTimeout()
 //Asynchronous is non-blocking architecture so the execution of the task inst's dependent on another.
+
+//1) instruction in asynchronous code can execute in parallel
+//2) next operation can occur while previous operation is still getting proccessed
+//3) Asynchronous javaScript preferred in situation in which exectuction gets blocked indefinitely
 
 console.log('First');
 // set timeout for 2 seconds
@@ -51,5 +68,25 @@ function generateRandom(min,max){
     console.log(num)
 })
 ();
+
+
+//an example of synchronous in javaScript
+const message = 'javaScript World';
+const greeting = 'I Love ${message}';
+console.log(greeting);
+
+
+console.og("Execution started");
+
+setTimeout(() => {
+    console.log("Inside setTimeout");
+}, 3000);
+
+console.log("Execution ended");
+
+
+
+
+
 
 
